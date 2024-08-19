a1ba9db9ce1222b77f3b9938dc261a3cd048a022
Pedro Braghin <braghindev@gmail.com>
2024-08-19 09:30:06 -0300

    feat: add new global constants and colors

--- src/@types/routes.ts
+++ src/@types/routes.ts
@@ -4,4 +4,5 @@ export enum Routes {
   profile = "(tabs)/profile",
   statistics = "(tabs)/statistics",
   createTraining = "create-training",
+  training = "training",
 }
--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -3,4 +3,7 @@ export default {
   black: "#000",
   white: "#fff",
   error: "#FF5858",
+  red: "#ff0000",
+  green: "#00ff00",
+  orange: "#F25511",
 };
