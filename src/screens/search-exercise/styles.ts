04a70f5131c7a48df5abaa93d9606715c4051fcf
Pedro Braghin <braghindev@gmail.com>
2024-08-19 09:31:49 -0300

    style: adjust search-exercise page padding

--- src/screens/search-exercise/styles.ts
+++ src/screens/search-exercise/styles.ts
@@ -4,7 +4,7 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingHorizontal: 26,
+    paddingHorizontal: 16,
     paddingTop: 28,
     backgroundColor: Colors.black,
     gap: 16,
